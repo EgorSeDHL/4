e87abecd2c23c936f7ddb15780eca83b31633456
EgorSeDHL <isip_e.d.sbrodov@mpt.ru>
2022-10-20 23:28:26 +0300

    first commit

---
+++ практическая работа №4/Note.cs
@@ -0,0 +1,13 @@
+﻿namespace практическая_работа__4
+{
+    internal class Note
+    {
+        public DateOnly firstnote;
+        public string name;
+        public string discript;
+        public DateOnly finaldate;
+    }
+
+
+}
+
